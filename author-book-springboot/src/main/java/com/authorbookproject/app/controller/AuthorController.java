a07e32ac71577e0302b04f69a033837fe4fff75f
SerenaPang4 <ypang4@usfca.edu>
2024-07-19 13:51:56 -0500

    consolidate author and book repo methods

--- author-book-springboot/src/main/java/com/authorbookproject/app/controller/AuthorController.java
+++ author-book-springboot/src/main/java/com/authorbookproject/app/controller/AuthorController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.authorbookproject.app.model.Author;
-import com.authorbookproject.app.model.Book;
 import com.authorbookproject.app.repository.AuthorRepository;
 
 @RestController
--- author-book-springboot/src/main/java/com/authorbookproject/app/repository/AuthorRepository.java
+++ author-book-springboot/src/main/java/com/authorbookproject/app/repository/AuthorRepository.java
@@ -11,7 +11,6 @@ import com.authorbookproject.app.model.Book;
 @Repository
 public class AuthorRepository {
 	List<Author> authors = new ArrayList<>();
-	List<Book> books = new ArrayList<>();
 
 	public AuthorRepository() {
 		System.out.println("AuthorRepository.AuthorRepository()");
@@ -63,57 +62,70 @@ public class AuthorRepository {
 		}
 		return author;
 	}
-	
+
 	public Book findBookById(Integer id) {
 		Book resultBook = null;
-		for (int i = 0; i < books.size(); i++) {
-			if (id == books.get(i).getId()) {
-				resultBook = books.get(i);
-				break;
+		for (Author author : authors) {
+			List<Book> books = author.getBooks();
+			for (int i = 0; i < books.size(); i++) {
+				if (id == books.get(i).getId()) {
+					resultBook = books.get(i);
+					break;
+				}
 			}
 		}
 		return resultBook;
 	}
-	
+
 	public List<Book> findAllBooks() {
+		List<Book> books = new ArrayList<>();
+		for (Author author : authors) {
+			books.addAll(author.getBooks());
+		}
 		return books;
 	}
-	
+
 	public Book deleteByBookId(Integer idBook) {
 		Book target = null;
-		for (int i = 0; i < books.size(); i++) {
-			if (idBook == books.get(i).getId()) {
-				target = books.get(i);
-				System.out.println("Deleting " + target.toString());
-				books.remove(i);
-				break;
+		for (Author author : authors) {
+			List<Book> books = author.getBooks();
+			for (int i = 0; i < books.size(); i++) {
+				if (idBook == books.get(i).getId()) {
+					target = books.get(i);
+					System.out.println("Deleting " + target.toString());
+					books.remove(i);
+					break;
+				}
 			}
 		}
 		return target;
 	}
-	
+
 	public Book updateBook(Book book) {
 		Book target = book;
-		for (int i = 0; i < books.size(); i++) {
-			if (target.getId() == books.get(i).getId()) {
-				books.get(i).setName(target.getName());
-				books.get(i).setIsbn(target.getIsbn());
-				break;
+		for (Author author : authors) {
+			List<Book> books = author.getBooks();
+			for (int i = 0; i < books.size(); i++) {
+				if (target.getId() == books.get(i).getId()) {
+					books.get(i).setName(target.getName());
+					books.get(i).setIsbn(target.getIsbn());
+					break;
+				}
 			}
 		}
-		return book;
+		return target;
+
 	}
-	
+
 	public Book saveBook(Integer idAuthor, Book book) {
-		System.out.println("BookController.saveBook() idAuthor " + idAuthor);
+		System.out.println("authorrepo.saveBook() idAuthor " + idAuthor);
 		for (Author author : authors) {
 			if (author.getId() == idAuthor) {
 				author.getBooks().add(book);
-				break;
-			} else {
-				return null;		
+				return book;
 			}
 		}
-		return book;
+		return null;
 	}
+
 }
