97dd3937244ca6228e7e8440d4fdc912351bee79
SerenaPang4 <ypang4@usfca.edu>
2024-07-25 13:04:50 -0500

    add jdbc save book Test:
    mvn package assembly:single
    
    java -classpath ./target/author-book-jdbc-app-1.0-SNAPSHOT-jar-with-dependencies.jar \
        com.authorbookjdbcproject.app.App  \
        ./src/main/java/com/authorbookjdbcproject/app/mysqldb.properties

--- authorbookjdbc-app/src/main/java/com/authorbookjdbcproject/app/App.java
+++ authorbookjdbc-app/src/main/java/com/authorbookjdbcproject/app/App.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.authorbookjdbcproject.app.dao.AuthorJdbcDao;
+import com.authorbookjdbcproject.app.dao.BookJdbcDao;
 import com.authorbookjdbcproject.app.dao.JdbcDataSource;
 import com.authorbookjdbcproject.app.model.Author;
 import com.authorbookjdbcproject.app.model.Book;
@@ -30,28 +31,32 @@ public class App
 			e.printStackTrace();
 		}
 		AuthorJdbcDao authorDao = new AuthorJdbcDao(jdbcDataSource);
-		List<Book> listOfBooks = new ArrayList<Book>();
-		List<Book> listOfBooks2 = new ArrayList<Book>();
-		List<Book> listOfBooks3 = new ArrayList<Book>();
-		Book b1 = new Book(0,"aaaa","cb6-ce948hjg-78");
-		Book b2 = new Book(1,"bbbbb","cb6-yjbv56-78");
-		Book b3 = new Book(2,"cccc","cb6-hb3456h-78");
-		listOfBooks.add(b1);
-		listOfBooks.add(b2);
-		listOfBooks.add(b3);
-		Author a1 = new Author("Milan",listOfBooks);
-		Author a2 = new Author("Murakami",listOfBooks2);
-		Author a3 = new Author("May",listOfBooks3);
-		
-		authorDao.saveAuthor(a1);
-		authorDao.saveAuthor(a2);
-		authorDao.saveAuthor(a3);
-		authorDao.findAuthorById(1);
+//		List<Book> listOfBooks = new ArrayList<Book>();
+//		List<Book> listOfBooks2 = new ArrayList<Book>();
+//		List<Book> listOfBooks3 = new ArrayList<Book>();
+//		Book b1 = new Book(0,"aaaa","cb6-ce948hjg-78");
+//		Book b2 = new Book(1,"bbbbb","cb6-yjbv56-78");
+//		Book b3 = new Book(2,"cccc","cb6-hb3456h-78");
+//		listOfBooks.add(b1);
+//		listOfBooks.add(b2);
+//		listOfBooks.add(b3);
+//		Author a1 = new Author("Milan",listOfBooks);
+//		Author a2 = new Author("Murakami",listOfBooks2);
+//		Author a3 = new Author("May",listOfBooks3);
+//		
+//		authorDao.saveAuthor(a1);
+//		authorDao.saveAuthor(a2);
+//		authorDao.saveAuthor(a3);
+//		authorDao.findAuthorById(1);
+//		authorDao.findAllAuthors();
+//		authorDao.deleteAuthor(2);
+//		authorDao.findAllAuthors();
+//		Author a4 = new Author("MayMayMay",listOfBooks3);
+//		authorDao.updateAuthor(a4);
 		authorDao.findAllAuthors();
-		authorDao.deleteAuthor(2);
-		authorDao.findAllAuthors();
-		Author a4 = new Author("MayMayMay",listOfBooks3);
-		authorDao.updateAuthor(a4);
+		BookJdbcDao bookDao = new BookJdbcDao(jdbcDataSource);
+		Book b1 = new Book(8,"Love Actually", "fgh-567gv-567");
+		bookDao.saveBook(3, b1);
 		authorDao.findAllAuthors();
     }
 }
--- authorbookjdbc-app/src/main/java/com/authorbookjdbcproject/app/dao/BookJdbcDao.java
+++ authorbookjdbc-app/src/main/java/com/authorbookjdbcproject/app/dao/BookJdbcDao.java
@@ -1,14 +1,54 @@
 package com.authorbookjdbcproject.app.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
+import com.authorbookjdbcproject.app.model.Author;
 import com.authorbookjdbcproject.app.model.Book;
 
-public class BookJdbcDao implements BookDao{
+public class BookJdbcDao implements BookDao {
+	private JdbcDataSource dataSource;
+	AuthorJdbcDao authorDao = null;
+
+	public BookJdbcDao(JdbcDataSource dataSource) {
+		this.dataSource = dataSource;
+		authorDao = new AuthorJdbcDao(dataSource);
+	}
 
 	@Override
 	public Book saveBook(Integer authorId, Book book) {
-		// TODO Auto-generated method stub
+		System.out.println("jdbc save book");
+		Author authorExist = authorDao.findAuthorById(authorId);
+		if (authorExist != null) {
+
+			if (authorExist.getBooks() == null || authorExist.getBooks().isEmpty()) {
+				try (Connection connection = dataSource.getConnection()) {
+					PreparedStatement ps = connection
+							.prepareStatement("INSERT INTO BOOK(name, isbn, id_author) " + "VALUES(?,?,?)");
+					ps.setString(1, book.getName());
+					ps.setString(2, book.getIsbn());
+					ps.setInt(3, authorId);
+					int i = ps.executeUpdate();
+
+					if (i == 1) {
+						System.out.println("jdbc saved book info to database");
+						System.out.println(book.toString());
+					}
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			} else {
+				List<Book> bookList = authorExist.getBooks();
+				for (int i = 0; i < bookList.size(); i++) {
+					if (book.getId() == bookList.get(i).getId() && book.getName() != bookList.get(i).getName()) {
+						System.out.println("Book already exist");
+						return null;
+					}
+				}
+			}
+		}
 		return null;
 	}
 
