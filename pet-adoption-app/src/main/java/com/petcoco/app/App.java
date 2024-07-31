e4f108df6b520bfdc33b3b9070649f0bffb89fc7
SerenaPang4 <ypang4@usfca.edu>
2024-07-31 12:22:09 -0500

    setting up commection for database

--- pet-adoption-app/src/main/java/com/petcoco/app/App.java
+++ pet-adoption-app/src/main/java/com/petcoco/app/App.java
@@ -1,13 +1,16 @@
 package com.petcoco.app;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Hello world!
- *
+ *  * Main class to start Spring Boot with JDBC support
  */
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+		SpringApplication.run(App.class, args);
     }
 }
---
+++ pet-adoption-app/src/main/java/com/petcoco/dao/JdbcDataSource.java
@@ -0,0 +1,62 @@
+package com.petcoco.dao;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * A simple data source for getting database connections.
+ */
+@Component
+public class JdbcDataSource {
+	private String propertiesFileName = "mysqldb.properties";
+	private static String url;
+	private static String username;
+	private static String password;
+
+	public JdbcDataSource() throws ClassNotFoundException, IOException, SQLException {
+		init(propertiesFileName);
+	}
+
+	/**
+	 * Initialize the data source
+	 * 
+	 * @param propertiesFile the name of the property file that contains the
+	 *                       database driver, URL, username, and password.
+	 *
+	 *                       Note: propertiesFile has to be in the same directory
+	 *                       that {@code JdbcDataSource}
+	 */
+	private void init(String propertiesFile) throws IOException, ClassNotFoundException {
+		Properties props = new Properties();
+		InputStream stream = JdbcDataSource.class.getResourceAsStream(propertiesFile);
+		props.load(stream);
+
+		String driver = props.getProperty("jdbc.driver");
+		url = props.getProperty("jdbc.url");
+		username = props.getProperty("jdbc.username");
+		password = props.getProperty("jdbc.password");
+
+		Class.forName(driver);
+
+		try(Connection con = getConnection()) {
+			System.out.println("Connected to " + con.getMetaData().getDatabaseProductName());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Gets a connection to the database
+	 * 
+	 * @return the database connection
+	 */
+	public Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(url, username, password);
+	}
+}
