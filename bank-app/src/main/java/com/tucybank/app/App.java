d4e6ba63fa7c6a30c38f8c2d1b70d1c51be5adf4
SerenaPang4 <ypang4@usfca.edu>
2024-08-06 14:53:41 -0700

    add maven dependencies for bank app

---
+++ bank-app/src/main/java/com/tucybank/app/App.java
@@ -0,0 +1,13 @@
+package com.tucybank.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
