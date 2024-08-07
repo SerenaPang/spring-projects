accee54f80878c660eca757560ea4e112beefbbe
SerenaPang4 <ypang4@usfca.edu>
2024-08-07 15:56:37 -0700

    add find activity by id, find all activities

--- bank-app/src/main/java/com/tucybank/app/dao/ActivityJdbcDao.java
+++ bank-app/src/main/java/com/tucybank/app/dao/ActivityJdbcDao.java
@@ -1,5 +1,11 @@
 package com.tucybank.app.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,14 +24,52 @@ public class ActivityJdbcDao implements ActivityDao{
 
 	@Override
 	public Activity findActivityById(Integer id) {
-		// TODO Auto-generated method stub
+		System.out.println("jdbc find Activity by id " + id);
+
+		try (Connection connection = dataSource.getConnection()) {
+			PreparedStatement ps = connection.prepareStatement("SELECT id_activity, id_account, date_activity, type_activity, amount FROM ACTIVITY WHERE id_activity =?");
+			ps.setInt(1, id);
+			Activity activity = new Activity();
+			try (ResultSet rs = ps.executeQuery()) {
+				if (rs.next()) {
+					activity.setIdActivity(rs.getInt("id_activity"));
+					activity.setIdAccount(rs.getInt("id_account"));
+					activity.setDate(rs.getDate("date_activity"));
+					activity.setType(rs.getString("type_activity"));
+					activity.setAmount(rs.getFloat("amount"));			
+					System.out.println(activity.toString());
+				}
+				return activity;
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public List<Activity> findAllActivities() {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("jdbc find all authors");
+		List<Activity> activities = new ArrayList<>();
+		try (Connection connection = dataSource.getConnection()) {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT id, name from ACTIVITY");
+
+			while (rs.next()) {
+				Activity activity = new Activity();
+				activity.setIdActivity(rs.getInt("id_activity"));
+				activity.setIdAccount(rs.getInt("id_account"));
+				activity.setDate(rs.getDate("date_activity"));
+				activity.setType(rs.getString("type_activity"));
+				activity.setAmount(rs.getFloat("amount"));	
+				activities.add(activity);
+			}
+			System.out.println(activities);
+			return activities;
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return authors;
 	}
 
 	@Override
--- bank-app/src/main/java/com/tucybank/app/model/Activity.java
+++ bank-app/src/main/java/com/tucybank/app/model/Activity.java
@@ -1,8 +1,14 @@
 package com.tucybank.app.model;
 
+import java.sql.Date;
+
 public class Activity {
+	private int idActivity;
+	private int idAccount;
+	private Date date;
 	private String type;
 	private float amount;
+	
 	public String getType() {
 		return type;
 	}
@@ -15,8 +21,27 @@ public class Activity {
 	public void setAmount(float amount) {
 		this.amount = amount;
 	}
+	public int getIdActivity() {
+		return idActivity;
+	}
+	public void setIdActivity(int idActivity) {
+		this.idActivity = idActivity;
+	}
+	public int getIdAccount() {
+		return idAccount;
+	}
+	public void setIdAccount(int idAccount) {
+		this.idAccount = idAccount;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
 	@Override
 	public String toString() {
-		return "Activity [type=" + type + ", amount=" + amount + "]";
+		return "Activity [idActivity=" + idActivity + ", idAccount=" + idAccount + ", date=" + date + ", type=" + type
+				+ ", amount=" + amount + "]";
 	}
 }
