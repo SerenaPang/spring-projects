34160375420ed009c61ede1b6e53d314422793ef
SerenaPang4 <ypang4@usfca.edu>
2024-08-07 15:58:56 -0700

    add delete activity jdbc

--- bank-app/src/main/java/com/tucybank/app/dao/ActivityJdbcDao.java
+++ bank-app/src/main/java/com/tucybank/app/dao/ActivityJdbcDao.java
@@ -10,12 +10,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.authorbookjdbcproject.app.model.Author;
 import com.tucybank.app.model.Activity;
 
-public class ActivityJdbcDao implements ActivityDao{
+public class ActivityJdbcDao implements ActivityDao {
 	@Autowired
 	private JdbcDataSource dataSource;
-	
+
 	@Override
 	public Activity saveActivity(Activity activity) {
 		// TODO Auto-generated method stub
@@ -27,7 +28,8 @@ public class ActivityJdbcDao implements ActivityDao{
 		System.out.println("jdbc find Activity by id " + id);
 
 		try (Connection connection = dataSource.getConnection()) {
-			PreparedStatement ps = connection.prepareStatement("SELECT id_activity, id_account, date_activity, type_activity, amount FROM ACTIVITY WHERE id_activity =?");
+			PreparedStatement ps = connection.prepareStatement(
+					"SELECT id_activity, id_account, date_activity, type_activity, amount FROM ACTIVITY WHERE id_activity =?");
 			ps.setInt(1, id);
 			Activity activity = new Activity();
 			try (ResultSet rs = ps.executeQuery()) {
@@ -36,7 +38,7 @@ public class ActivityJdbcDao implements ActivityDao{
 					activity.setIdAccount(rs.getInt("id_account"));
 					activity.setDate(rs.getDate("date_activity"));
 					activity.setType(rs.getString("type_activity"));
-					activity.setAmount(rs.getFloat("amount"));			
+					activity.setAmount(rs.getFloat("amount"));
 					System.out.println(activity.toString());
 				}
 				return activity;
@@ -53,7 +55,7 @@ public class ActivityJdbcDao implements ActivityDao{
 		List<Activity> activities = new ArrayList<>();
 		try (Connection connection = dataSource.getConnection()) {
 			Statement stmt = connection.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT id, name from ACTIVITY");
+			ResultSet rs = stmt.executeQuery("SELECT id_activity, id_account, date_activity, type_activity, amount from ACTIVITY");
 
 			while (rs.next()) {
 				Activity activity = new Activity();
@@ -61,7 +63,7 @@ public class ActivityJdbcDao implements ActivityDao{
 				activity.setIdAccount(rs.getInt("id_account"));
 				activity.setDate(rs.getDate("date_activity"));
 				activity.setType(rs.getString("type_activity"));
-				activity.setAmount(rs.getFloat("amount"));	
+				activity.setAmount(rs.getFloat("amount"));
 				activities.add(activity);
 			}
 			System.out.println(activities);
@@ -69,12 +71,27 @@ public class ActivityJdbcDao implements ActivityDao{
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
-		return authors;
+		return activities;
 	}
 
 	@Override
 	public Activity deleteActivity(Integer idActivity) {
-		// TODO Auto-generated method stub
+		System.out.println("jdbc delete Activity");
+
+		Activity target = findActivityById(idActivity);
+		if (target != null) {
+			try (Connection connection = dataSource.getConnection()) {
+				PreparedStatement ps = connection.prepareStatement("DELETE FROM ACTIVITY WHERE id_activity= ?");
+				ps.setInt(1, idActivity);
+				int i = ps.executeUpdate();
+				if (i == 1) {
+					System.out.println("jdbc delete Activity " + target.toString());
+					return target;
+				}
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		}
 		return null;
 	}
 
