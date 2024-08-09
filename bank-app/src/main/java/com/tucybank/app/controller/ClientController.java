7ae67b790dc02f67e6e25e03ab35ce33d81ca780
SerenaPang4 <ypang4@usfca.edu>
2024-08-08 20:07:26 -0700

    add delete client Test: curl -H 'Content-Type: application/json' -X DELETE http://localhost:8080/deleteClient/1

--- bank-app/src/main/java/com/tucybank/app/controller/ClientController.java
+++ bank-app/src/main/java/com/tucybank/app/controller/ClientController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -57,8 +59,16 @@ public class ClientController {
 		return ResponseEntity.status(HttpStatus.OK).body(clients);
 	}
 	
-	
-	
+	// curl -H 'Content-Type: application/json' -X DELETE http://localhost:8080/deleteClient/1
+	@DeleteMapping(path = "/deleteClient/{idCilent}")
+	public ResponseEntity<Client> deleteClient(@PathVariable(name = "idCilent") Integer idCilent) {
+		Client client = clientJdbcDao.findClientById(idCilent);
+		if (client == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		clientJdbcDao.deleteClient(idCilent);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
 	
 	
 	
