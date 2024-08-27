a92de2c01bf83ebb652a6a7af98f40f034083566
SerenaPang4 <ypang4@usfca.edu>
2024-08-27 11:08:06 -0700

    add web services for: findby id, update order Test: mvn spring-boot:run curl -X GET "http://localhost:8080/findById?id=1"
    curl -H 'Content-Type: application/json' -d '{ "orderId":"1", "userId":"1","totalPrice":"20", "discount":"0", "saleDate":"2024-08-30  09:15:07","status":"ORDER PROCCSING"}' -X PUT http://localhost:8080/update

--- music-app/src/main/java/com/tucymusic/app/controller/OrderController.java
+++ music-app/src/main/java/com/tucymusic/app/controller/OrderController.java
@@ -2,13 +2,18 @@ package com.tucymusic.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tucymusic.app.dao.jdbc.OrderDaoImpl;
 import com.tucymusic.app.model.Order;
+import com.tucymusic.app.model.Product;
 
 @RestController
 public class OrderController {
@@ -26,6 +31,29 @@ public class OrderController {
 			return ResponseEntity.status(HttpStatus.CREATED).body(order);
 		};
 		
+		// curl -H 'Content-Type: application/json' -d '{ "orderId":"1", "userId":"1","totalPrice":"20", "discount":"0", "saleDate":"2024-08-30  09:15:07","status":"ORDER PROCCSING"}' -X PUT http://localhost:8080/update
+		@PutMapping(path = "/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+		public ResponseEntity<Order> update(@RequestBody Order order) {
+			Integer orderId = order.getOrderId();
+			Order target = jdbcOrderDao.findById(orderId);
+			if (target == null) {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			}
+			jdbcOrderDao.update(order);
+			// Return the updated resource with a 200 (OK) status code
+			return ResponseEntity.status(HttpStatus.OK).body(order);
+		};
 		
+		// curl -X GET "http://localhost:8080/findById?id=1"
+		@GetMapping("/findById")
+		public ResponseEntity<Order> findById(@RequestParam(value = "id", defaultValue = "0") int id) {
+			System.out.println("OrderController.findById() " + id);
+			Order resultOrder = jdbcOrderDao.findById(id);
+			if (resultOrder == null) {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			}
+			// Return the resource with a 200 (OK) status code
+			return ResponseEntity.status(HttpStatus.OK).body(resultOrder);
+		};
 		
 }
