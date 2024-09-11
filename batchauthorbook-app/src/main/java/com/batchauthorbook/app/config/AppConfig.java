ca96f98c22dbc336aad74cb7f7a10ea382dc0c78
SerenaPang4 <ypang4@usfca.edu>
2024-09-10 17:28:33 -0700

    add app configuration

---
+++ batchauthorbook-app/src/main/java/com/batchauthorbook/app/config/AppConfig.java
@@ -0,0 +1,28 @@
+package com.batchauthorbook.app.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.batchauthorbook.app.repo.BatchAuthorRepository;
+import com.batchauthorbook.app.repo.SimpleAuthorRepository;
+import com.batchauthorbook.app.service.AuthorService;
+
+import java.time.Clock;
+import java.util.Random;
+
+@Configuration
+@PropertySource("classpath:com/batchauthorbook/app/application.properties")
+public class AppConfig {
+
+    @Bean
+    public AuthorService simpleProductService(SimpleAuthorRepository simpleAuthorRepository) {
+        return new AuthorService(simpleAuthorRepository, new Random(), Clock.systemUTC());
+    }
+
+    @Bean
+    public AuthorService batchProductService(BatchAuthorRepository batchAuthorRepository) {
+        return new AuthorService(batchAuthorRepository, new Random(), Clock.systemUTC());
+    }
+}
