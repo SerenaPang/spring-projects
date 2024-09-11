1e31b19dbde4bd2a01035c53c28eee190600a535
SerenaPang4 <ypang4@usfca.edu>
2024-09-10 17:21:47 -0700

    add book service

--- batchauthorbook-app/src/main/java/com/batchauthorbook/app/service/BookService.java
+++ batchauthorbook-app/src/main/java/com/batchauthorbook/app/service/BookService.java
@@ -1,10 +1,44 @@
 package com.batchauthorbook.app.service;
 
-public class BookService {
+import java.time.Clock;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-	public long createBooks(int recordCount) {
-		// TODO Auto-generated method stub
-		return 0;
+import com.batchauthorbook.app.model.Book;
+import com.batchauthorbook.app.repo.BookRepository;
+
+public class BookService {
+	
+	private BookRepository bookRepository;
+	private Random random;
+	private Clock clock;
+
+	// constructor for the dependencies
+	public BookService(BookRepository bookRepository, Random random, Clock clock) {
+		this.bookRepository = bookRepository;
+		this.random = random;
+		this.clock = clock;
+	}
+
+	private List<Book> generate(int count) {
+		final String[] names = { "a", "b", "c", "d" };
+		final List<Book> books = new ArrayList<>(count);
+
+		for (int i = 0; i < count; i++) {
+			Book book = new Book();
+			book.setId(i);
+			book.setName(names[random.nextInt(4)]);
+			book.setIsbn("");
+			books.add(book);
+		}
+		return books;
+	}
+	public long createBooks(int count) {
+		List<Book> books = generate(count);
+		long startTime = clock.millis();
+		bookRepository.saveAll(books);
+		return clock.millis() - startTime;
 	}
 
 }
