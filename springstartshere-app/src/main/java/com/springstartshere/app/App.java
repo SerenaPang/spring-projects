35cbb712a006babfee516443df0e56f34a827cd1
SerenaPang4 <ypang4@usfca.edu>
2024-09-26 16:49:51 -0700

    defining the main mrthod to test the spring configuration

--- springstartshere-app/src/main/java/com/springstartshere/app/App.java
+++ springstartshere-app/src/main/java/com/springstartshere/app/App.java
@@ -1,13 +1,26 @@
 package com.springstartshere.app;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.google.common.base.Supplier;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+		Parrot x = new Parrot();
+		x.setName("Kiki");
+
+		Supplier<Parrot> parrotSupplier = () -> x;
+		context.registerBean("kiki", Parrot.class, parrotSupplier);
+
+		Parrot p = context.getBean(Parrot.class);
+		System.out.println(p.getName());
+		
+		//context.registerBean("parrot1", Parrot.class, parrotSupplier, bc -> bc.setPrimary(true));
+	}
 }
