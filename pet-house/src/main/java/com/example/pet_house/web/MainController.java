f17dafca80233885e33359ff6e0b34b147372c1c
SerenaPang4 <ypang4@usfca.edu>
2024-11-11 13:03:50 -0800

    update cats.html

--- pet-house/src/main/java/com/example/pet_house/web/MainController.java
+++ pet-house/src/main/java/com/example/pet_house/web/MainController.java
@@ -1,14 +1,46 @@
 package com.example.pet_house.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.example.pet_house.model.Cat;
 
 @Controller
 public class MainController {
+	private final CatService catService;
+	
+	public MainController(CatService catService) {
+		this.catService = catService;
+	}
+	
 	@RequestMapping("/home")
 	public String home(Model page) {
 		page.addAttribute("username", "Kity");
 		page.addAttribute("color", "blue");
 		return "home.html";
+	}
+	
+	@GetMapping("/cats")
+	public String viewCats(Model model) {
+		var cats = catService.findAll();
+		model.addAttribute("cats", cats);
+		return "cats.html";
+	}
+	
+	@PostMapping("/cats")
+	public String addCat(@RequestParam int id, @RequestParam String name, int age, String breed, String description, Model model) {
+		Cat c = new Cat();
+		c.setId(id);
+		c.setName(name);
+		c.setAge(age);
+		c.setBreed(breed);
+		c.setDescription(description);
+		catService.addCat(c);
+		
+		var cats = catService.findAll();
+		model.addAttribute("cats", cats);
+		return "cats.html";
 	}
 }
