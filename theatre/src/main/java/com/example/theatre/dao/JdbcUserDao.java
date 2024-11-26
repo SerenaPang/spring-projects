fd1f6008a35b2633e60b11fa93fe8ab4db72c680
SerenaPang4 <ypang4@usfca.edu>
2024-11-25 16:21:44 -0800

    add find user by id

--- theatre/src/main/java/com/example/theatre/dao/JdbcUserDao.java
+++ theatre/src/main/java/com/example/theatre/dao/JdbcUserDao.java
@@ -2,6 +2,7 @@ package com.example.theatre.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -41,7 +42,23 @@ public class JdbcUserDao implements UserDao{
 
 	@Override
 	public User findUserById(Integer id) {
-		// TODO Auto-generated method stub
+		System.out.println("jdbc find user by id " + id);
+		try (Connection connection = dataSource.getConnection()) {
+			PreparedStatement ps = connection
+					.prepareStatement("SELECT id, name FROM User WHERE id =?");
+			ps.setInt(1, id);
+			User user = new User();
+			try (ResultSet rs = ps.executeQuery()) {
+				if (rs.next()) {
+					user.setId(rs.getInt("id"));
+					user.setName(rs.getString("name"));
+					System.out.println(user.toString());
+				}
+				return user;
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
 		return null;
 	}
 
