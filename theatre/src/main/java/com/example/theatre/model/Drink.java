82e66c7b3ea8b1c06081fc2516f0e935c1d407e0
SerenaPang4 <ypang4@usfca.edu>
2024-11-25 17:00:45 -0800

    added food and drink model class

---
+++ theatre/src/main/java/com/example/theatre/model/Drink.java
@@ -0,0 +1,29 @@
+package com.example.theatre.model;
+
+public class Drink {
+	private int id;
+	private int name;
+	private double price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getName() {
+		return name;
+	}
+	public void setName(int name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Drink [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
+}
