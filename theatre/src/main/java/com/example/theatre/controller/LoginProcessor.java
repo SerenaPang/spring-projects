e50aff4413645ca3485a87542d808477011dccb3
SerenaPang4 <ypang4@usfca.edu>
2024-11-26 17:10:16 -0800

    added loggeduser management service

--- theatre/src/main/java/com/example/theatre/controller/LoginProcessor.java
+++ theatre/src/main/java/com/example/theatre/controller/LoginProcessor.java
@@ -28,5 +28,12 @@ private String getPassword() {
 private String getUsername() {
 	return username;
 }
- 
+
+public void setUsername(String username) {
+	this.username = username;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
 }
