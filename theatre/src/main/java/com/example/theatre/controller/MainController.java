9789771fcc4d58591490de3b5eb7696084b77e58
SerenaPang4 <ypang4@usfca.edu>
2024-11-27 16:42:36 -0800

    updated main controller

--- theatre/src/main/java/com/example/theatre/controller/MainController.java
+++ theatre/src/main/java/com/example/theatre/controller/MainController.java
@@ -42,16 +42,6 @@ public class MainController {
 		return "login.html";
 	}
 
-	@PostMapping("/")
-	public String loginPost(@RequestParam String username, @RequestParam String password, Model model) {
-		boolean loggedIn = false;
-		if (loggedIn) {
-			model.addAttribute("message", "You are now logged in.");
-		} else {
-			model.addAttribute("message", "Login failed");
-		}
-		return "login.html";
-	}
 
 //	//redirect the user to the main page after login
 //	@PostMapping("/")
@@ -67,4 +57,47 @@ public class MainController {
 //		model.addAttribute("message", "Login failed!");
 //		return "login.html";
 //	}
+	
+	@RequestMapping("/home")
+	public String home(Model page) {
+		page.addAttribute("username", "Kity");
+		page.addAttribute("color", "blue");
+		return "home.html";
+	}
+	
+	@RequestMapping("/theater")
+	public String theater(Model page) {
+		page.addAttribute("username", "Kity");
+		page.addAttribute("color", "blue");
+		return "theater.html";
+	}
+	
+	@RequestMapping("/findtheaterbyzipcode")
+	public String findTheater(Model page) {
+		page.addAttribute("username", "Kity");
+		page.addAttribute("color", "blue");
+		return "findtheaterbyzipcode.html";
+	}
+	
+	
+	@RequestMapping("/movies")
+	public String movies(Model page) {
+		page.addAttribute("username", "Kity");
+		page.addAttribute("color", "blue");
+		return "movies.html";
+	}
+	
+	@RequestMapping("/food")
+	public String food(Model page) {
+		page.addAttribute("username", "Kity");
+		page.addAttribute("color", "blue");
+		return "food.html";
+	}
+	
+	@RequestMapping("/tickets")
+	public String tickets(Model page) {
+		page.addAttribute("username", "Kity");
+		page.addAttribute("color", "blue");
+		return "tickets.html";
+	}
 }
