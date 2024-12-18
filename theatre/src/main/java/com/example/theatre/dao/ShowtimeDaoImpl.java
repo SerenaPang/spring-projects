bf7df2f8e1f1fbad3f733df240b08625385ec0dd
SerenaPang4 <ypang4@usfca.edu>
2024-12-17 18:05:39 -0600

    updated showtime dao impl

--- theatre/src/main/java/com/example/theatre/dao/ShowtimeDaoImpl.java
+++ theatre/src/main/java/com/example/theatre/dao/ShowtimeDaoImpl.java
@@ -22,79 +22,19 @@ public class ShowtimeDaoImpl implements ShowtimeDao {
 	@Autowired
 	private JdbcDataSource dataSource;
 
-//	@Override
-//	public Map<String, List<Date>> findShowtimeByMovieName() {
-//		System.out.println("jdbc find all showtimes with movie names");
-//		Map<String, List<Date>> showtimes = new HashMap<String, List<Date>>();
-//
-//		try (Connection connection = dataSource.getConnection()) {
-//			PreparedStatement ps = connection.prepareStatement(
-//					"select m.movie_name, s.showtime "
-//					+ "from movie m, showtime s "
-//					+ "where s.movie_id = m.movie_id and s.showtime >= CURDATE() "
-//					+ "order by m.movie_name, s.showtime");
-//
-//			try (ResultSet rs = ps.executeQuery()) {
-//				while (rs.next()) {
-//					Showtime showtime = new Showtime();
-//					List<Date> listShowtimes;
-//					showtime.setName(rs.getString("movie_name"));
-//					showtime.setShowtime(rs.getTimestamp("showtime"));
-//					String name = showtime.getName();
-//					if (showtimes.containsKey(name)) {
-//						listShowtimes = showtimes.get(name);
-//						listShowtimes.add(showtime.getShowtime());
-//					} else {
-//						listShowtimes = new ArrayList<Date>();
-//						listShowtimes.add(showtime.getShowtime());
-//						showtimes.put(name, listShowtimes);
-//					}
-//				}
-//				System.out.println(showtimes.toString());
-//			}
-//		} catch (SQLException ex) {
-//			ex.printStackTrace();
-//		}
-//		return showtimes;
-//	}
-	
-//	@Override
-//	public List<Showtime> findShowtimeByMovieName() {
-//		System.out.println("jdbc find all showtimes with movie names");
-//		List<Showtime> showtimes = new ArrayList<>();
-//		try (Connection connection = dataSource.getConnection()) {
-//			PreparedStatement ps = connection
-//					.prepareStatement("select m.movie_name, s.showtime from movie m, showtime s");
-//			
-//			try (ResultSet rs = ps.executeQuery()) {
-//				while (rs.next()) {
-//					Showtime showtime = new Showtime();
-//					showtime.setName(rs.getString("movie_name"));
-//					showtime.setShowtime(rs.getDate("showtime"));
-//					showtimes.add(showtime);
-//				}			
-//				System.out.println(showtimes.toString());
-//				
-//				return showtimes;
-//			}
-//		} catch (SQLException ex) {
-//			ex.printStackTrace();
-//		}
-//		return null;
-//	}
-
 	@Override
 	public List<Showtime> findAllShowtimes() {
 		System.out.println("jdbc find all showtimes");
 		List<Showtime> showtimes = new ArrayList<>();
 		try (Connection connection = dataSource.getConnection()) {
-			PreparedStatement ps = connection.prepareStatement("select showtime_id, theater_id, showtime from SHOWTIME");
+			PreparedStatement ps = connection
+					.prepareStatement("select showtime_id, theater_id, showtime from SHOWTIME");
 
 			try (ResultSet rs = ps.executeQuery()) {
 				while (rs.next()) {
 					Showtime showtime = new Showtime();
-					//showtime.setMovieId(rs.getInt("movie_id"));
-					//showtime.setTheaterId(rs.getInt("theater_id"));
+					// showtime.setMovieId(rs.getInt("movie_id"));
+					// showtime.setTheaterId(rs.getInt("theater_id"));
 					showtime.setShowtime(rs.getDate("showtime"));
 					showtimes.add(showtime);
 
@@ -117,9 +57,9 @@ public class ShowtimeDaoImpl implements ShowtimeDao {
 		try (Connection connection = dataSource.getConnection()) {
 			PreparedStatement ps = connection.prepareStatement(
 					"select s.showtime_id, s.showtime, m.movie_id, m.movie_name, m.price, t.theater_id, t.zipcode, t.city "
-					+ "from movie m, showtime s, theater t "
-					+ "where m.movie_id = s.movie_id and s.theater_id = t.theater_id "
-					+ "and m.movie_id = ? and s.showtime >= CURDATE() order by t.theater_id;");
+							+ "from movie m, showtime s, theater t "
+							+ "where m.movie_id = s.movie_id and s.theater_id = t.theater_id "
+							+ "and m.movie_id = ? and s.showtime >= CURDATE() order by t.theater_id;");
 			ps.setInt(1, id);
 
 			try (ResultSet rs = ps.executeQuery()) {
@@ -135,7 +75,7 @@ public class ShowtimeDaoImpl implements ShowtimeDao {
 					Showtime showtime = new Showtime();
 					showtime.setShowtime_id(rs.getInt("showtime_id"));
 					showtime.setShowtime(rs.getDate("showtime"));
-							
+
 					showtimes.add(showtime);
 				}
 
