76f6f40ac03224b3ae5356dc1e936f23987ec5c6
SerenaPang4 <ypang4@usfca.edu>
2024-12-19 12:59:44 -0600

    added ordritem dao impl and orders dao impl

--- theatre/src/main/java/com/example/theatre/dao/OrderItemDaoImpl.java
+++ theatre/src/main/java/com/example/theatre/dao/OrderItemDaoImpl.java
@@ -1,15 +1,44 @@
 package com.example.theatre.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import com.example.theatre.model.OrderItem;
 
+@Repository
 public class OrderItemDaoImpl implements OrderItemDao{
+	@Autowired
+	private JdbcDataSource dataSource;
 
 	@Override
-	public List<OrderItem> findAllMyOrderItems(int userId) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<OrderItem> findAllMyOrderItems(int orderId) {
+		System.out.println("jdbc find all user's one order's order items");
+		List<OrderItem> orderItems = new ArrayList<>();
+		try (Connection connection = dataSource.getConnection()) {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT order_item_id, order_id, ticket_id, food_id, drink_id, quantity, price from ORDER_ITEM");
+
+			while (rs.next()) {
+				OrderItem orderItem = new OrderItem();
+				orderItem.setOrderItemId(rs.getInt(""));
+				orderItem.setOrderId(rs.getInt(""));
+				orderItem.setTicket(null);
+				
+				
+				orderItems.add(orderItem);
+			}
+			System.out.println(orderItems);
+			return orderItems;
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return orderItems;
 	}
 
 }
--- theatre/src/main/java/com/example/theatre/dao/OrdersDaoImpl.java
+++ theatre/src/main/java/com/example/theatre/dao/OrdersDaoImpl.java
@@ -1,13 +1,40 @@
 package com.example.theatre.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import com.example.theatre.model.Order;
 
-public class OrdersDaoImpl implements OrdersDao{
+@Repository
+public class OrdersDaoImpl implements OrdersDao {
+	@Autowired
+	private JdbcDataSource dataSource;
 
 	@Override
 	public Order findMyOrder(int userId) {
-		// TODO Auto-generated method stub
+		System.out.println("jdbc find order by id " + userId);
+		try (Connection connection = dataSource.getConnection()) {
+			PreparedStatement ps = connection.prepareStatement(
+					"SELECT order_id, user_id, total_price, sale_date, status FROM ORDERS WHERE user_id =?");
+			ps.setInt(1, userId);
+			Order order = new Order();
+			try (ResultSet rs = ps.executeQuery()) {
+				if (rs.next()) {
+					order.setOrderId(rs.getInt("order_id"));
+					order.setUserId(rs.getInt("user_id"));
+					order.setTotalPrice(rs.getDouble("total_price"));
+					order.setSaleDate(rs.getDate("sale_date"));
+					order.setStatus(rs.getString("status"));
+					System.out.println(order.toString());
+				}
+				return order;
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
 		return null;
 	}
-
 }
